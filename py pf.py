c95307972520e1acdb50bc64cc603e2bbbfd7b9a
FadelSM <rasyahstore8888@gmail.com>
2026-02-11 10:13:48 +0700

    Update and rename py.py to py pf.py

---
+++ py pf.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    n = int(input())
+    
+    # Loop from 1 to n (inclusive)
+    for i in range(1, n + 1):
+        # Print each number followed by an empty string instead of a newline
+        print(i, end="")
